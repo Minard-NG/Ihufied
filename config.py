ee39a772ba0fe427e2a14c88437b84f58ced8ae9
Michael <michael4sure20@gmail.com>
2021-05-05 11:39:03 +0100

    changed email port

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.gmail.com')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT', '465'))
+    MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
     MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL', 'true').lower() in ['true', 'on','1']
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
