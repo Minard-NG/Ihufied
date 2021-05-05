ab76235c48b060edb2bc85cfcbe5d2b27d0db92c
Michael <michael4sure20@gmail.com>
2021-05-05 12:23:06 +0100

    switched emails from google to shop4nysc

--- config.py
+++ config.py
@@ -4,12 +4,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.gmail.com')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
+    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'mail.shop4nysc.com')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT', '465'))
     MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL', 'true').lower() in ['true', 'on','1']
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    MAIL_DEFAULT_SENDER = 'nodywelete3@gmail.com'
+    MAIL_DEFAULT_SENDER = 'ihufied@shop4nysc.com'
     FLASKY_MAIL_SUBJECT_PREFIX= '[IHUFIED]'
     FLASKY_MAIL_SENDER = 'FLasky Admin <flasky@example.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
