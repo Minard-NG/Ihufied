ec2117a456d441570156467aace696003b162afd
Michael <michael4sure20@gmail.com>
2021-05-06 14:16:14 +0100

    made changes

--- app/__init__.py
+++ app/__init__.py
@@ -17,8 +17,8 @@ db = SQLAlchemy()
 
 #def create_app(config_name):
 app = Flask(__name__)
-app.config.from_object(config[config_name])
-config[config_name].init_app(app)
+app.config.from_object(config['default'])
+config['default'].init_app(app)
 
 bootstrap.init_app(app)
 moment.init_app(app)
