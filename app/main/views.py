1c9c307254db127d7aed25fe27f1263a0ef5bbe2
Michael <michael4sure20@gmail.com>
2021-05-07 15:17:26 +0100

    minor fix

--- app/main/views.py
+++ app/main/views.py
@@ -16,26 +16,26 @@ def index():
 
 @main.route('/getuser/<string:course_title>')
 def getuser(course_title):
-	try:
-		course_title= course_title.upper()
-		course = Course.query.filter_by(title=course_title).first()
+    try:
+        course_title= course_title.upper()
+        course = Course.query.filter_by(title=course_title).first()
         print(course)
-		users = course.course_subscribers
-		students_list = []
-		for student in users:
-			students = {}
-			students['firstname'] = student.firstname
-			students['lastname'] = student.lastname
-			students['reg_no'] = student.regnumber
-			#convert the students image stored in bytes to str so it can be passable with json on line 41
-			students['img'] = student.image.decode('utf-8') 
-			students_list.append(students)
-		if students_list == []:
-			return 'No student registered!'
-		else:
-			return json.dumps(students_list)
-	except AttributeError:
-		return 'Course not found!'
+        users = course.course_subscribers
+        students_list = []
+        for student in users:
+            students = {}
+            students['firstname'] = student.firstname
+            students['lastname'] = student.lastname
+            students['reg_no'] = student.regnumber
+            #convert the students image stored in bytes to str so it can be passable with json on line 41
+            students['img'] = student.image.decode('utf-8') 
+            students_list.append(students)
+        if students_list == []:
+            return 'No student registered!'
+        else:
+            return json.dumps(students_list)
+    except AttributeError:
+        return 'Course not found!'
 
 #getting only students in that department registered can be done by inserting the id into the routing channel
 
